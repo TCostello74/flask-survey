524a789b805ef38e0973a547bc6fe062a1e8d6c2
tcostello74 <tcostello74@yahoo.com>
2023-04-22 10:32:24 -0400

    steps 6-7

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = "Tyler"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
 debug = DebugToolbarExtension(app)
 
 
@@ -18,7 +18,7 @@ def survey_start():
     """Root and start of survey"""
     return render_template("survey_start.html", survey=survey)
 
-app.route('/start', methods=["POST"])
+@app.route("/start", methods=["POST"])
 def start_quest():
     
     return redirect("/questions/0")
@@ -27,13 +27,40 @@ def start_quest():
 def show_answers():
 
     choice = request.form["answer"]
+    
     responses.append(choice)
+
+    if (len(responses) == len(survey.questions)):
+        # They've answered all the questions! Thank them.
+        return redirect("/complete")
+
+    else:
+        return redirect(f"/questions/{len(responses)}")
 
 @app.route('/questions/<int:idx>')
 def show_question(idx):
     "Show questions"
+    if (responses is None):
+        # trying to access question page too soon
+        return redirect("/")
+
+    if (len(responses) == len(survey.questions)):
+        # They've answered all the questions! Thank them.
+        return redirect("/complete")
+
+    if (len(responses) != idx):
+        # Trying to access questions out of order.
+        flash(f"Invalid question id: {idx}.")
+        return redirect(f"/questions/{len(responses)}")
+    
     question = survey.questions[idx]
     return render_template('questions.html', idx = idx, question = question)
+
+@app.route('/complete')
+def complete_page():
+    """Survey is complete page"""
+
+    return render_template("complete.html")
 
 
 
